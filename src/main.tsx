e65bd258250e4e391f43883541e661361549f7b2
grazialencar <graziellealencar3@gmail.com>
2025-10-05 13:48:09 -0300

    Acrescentado rotas na main e também o index.css

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,25 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 
+import Login from './routes/Login'
+import Cadastro from './routes/Cadastro'
+import Error from './routes/Error'
+import App from './App'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/login", element: <App />, errorElement: <Error />, children: [
+      {path: "/login", element: <Login/>},
+      {path: "cadastro", element: <Cadastro/>}
+    ]}
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
