e4761bab292dccd00d75626aae985bc01fc6de07
diandrade <comtato.andradediego@gmail.com>
2025-10-07 20:39:32 -0300

    Interface adicionada para a utilização do Props

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,6 +1,16 @@
 
-
+import type { TipoUsuario } from "../../types/tipoUsu";
+ 
+interface FormProps {
+    onSubmit: (data: TipoUsuario) => void;
+    fields: Array<'nome' | 'nomeUsuario' | 'email'>;
+    buttonText: string;
+    legend: string;
+}
+ 
+ 
 export default function Form() {
+ 
     return (
         <div></div>
     )
