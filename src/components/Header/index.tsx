33b2ca3d7f280d431431d8e66a935fe5c26e8705
grazialencar <graziellealencar3@gmail.com>
2025-10-07 21:56:17 -0300

    Componente header criado

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Header() {
+    return (
+        <header className="w-full bg-pink-600 text-white p-4">
+            <h1 className="text-lg font-semibold">Controle de Acesso</h1>
+        </header>
+    )
+}
