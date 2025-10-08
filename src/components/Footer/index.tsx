ce70fc1e6a6cfe9413296259d080cb67e8cc5bdb
grazialencar <graziellealencar3@gmail.com>
2025-10-07 21:56:01 -0300

    Componente footer adicionado

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Footer() {
+    return (
+        <footer className="w-full bg-gray-200 text-center p-4 mt-4">
+            <p className="text-sm text-gray-600">© 2024 Controle de Acesso. Todos os direitos reservados.</p>
+        </footer>
+    )
+}
