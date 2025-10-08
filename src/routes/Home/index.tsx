0230d4d9a190fd7623e23e80959a176bba5e1779
IsabelaYamauchi <isa.yamauchi.y@gmail.com>
2025-10-07 21:44:50 -0300

    Validando se usuário está logado com sessionStorage

--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -1,7 +1,38 @@
-
-
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom"
+import type { TipoUsuario } from "../../types/tipoUsu";
+ 
+ 
 export default function Home() {
+ 
+    const [usuario, setUsuario] = useState<TipoUsuario | null>(null);
+ 
+    const navigate = useNavigate();
+ 
+    useEffect(() => {
+        const usuarioLogado = sessionStorage.getItem('usuarioLogado');
+ 
+        if (usuarioLogado) {
+            setUsuario(JSON.parse(usuarioLogado));
+        } else {
+            alert("Acesso não autorizado. Por favor, faça o login.");
+            navigate("/");
+        }
+    }, [navigate]);
+ 
+    const handleLogout = () => {
+        sessionStorage.removeItem('usuarioLogado');
+        navigate("/");
+    }
+ 
+    if (!usuario) {
+        return null
+    }
+ 
+ 
     return (
-        <div></div>
+        <header className="flex flex-col min-h-screen bg-gray-50">
+            
+        </header>
     )
 }
