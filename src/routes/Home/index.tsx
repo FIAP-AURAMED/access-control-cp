fa9ba897cba58a3a975f35e1c88f9202f4122e6b
IsabelaYamauchi <isa.yamauchi.y@gmail.com>
2025-10-07 21:46:27 -0300

    Puxando os dados do usuário e botão de logout.

--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -32,7 +32,20 @@ export default function Home() {
  
     return (
         <header className="flex flex-col min-h-screen bg-gray-50">
-            
+            <div className="flex items-center justify-between w-full p-8 mx-auto bg-pink-700 shadow-lg">
+                <div>
+                    <h1 className="text-3xl font-bold text-white">
+                        Bem-vindo, {usuario.nome}!
+                    </h1>
+                    <p className="mt-2 text-white">Seu e-mail de acesso é: {usuario.email}</p>
+                </div>
+                <button
+                    onClick={handleLogout}
+                    className="px-4 py-2 text-sm font-medium text-red-600 bg-white border border-transparent rounded-md shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
+                >
+                    Sair
+                </button>
+            </div>
         </header>
     )
 }
