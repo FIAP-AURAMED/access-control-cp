92da28e0eedd3c8519399dd0b4eb7e5c65ec9d02
IsabelaYamauchi <isa.yamauchi.y@gmail.com>
2025-10-07 21:49:41 -0300

    Adicionado mensagem de error 404

--- src/routes/Error/index.tsx
+++ src/routes/Error/index.tsx
@@ -1,8 +1,13 @@
-
-
+import { useNavigate } from "react-router-dom";
+ 
+ 
 export default function Error() {
+ 
+    const navigate = useNavigate();
+ 
     return (
-        <div>
+        <div className="flex flex-col gap-4 items-center justify-center min-h-screen bg-gray-100">
+            <p>Erro 404: Página não encontrada</p>
             
         </div>
     )
