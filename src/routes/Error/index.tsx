8e0a69029cd937fa6fceadfa85c03845d3255eb7
IsabelaYamauchi <isa.yamauchi.y@gmail.com>
2025-10-07 21:50:16 -0300

    Botão de redirecionamento para o login no error

--- src/routes/Error/index.tsx
+++ src/routes/Error/index.tsx
@@ -8,7 +8,7 @@ export default function Error() {
     return (
         <div className="flex flex-col gap-4 items-center justify-center min-h-screen bg-gray-100">
             <p>Erro 404: Página não encontrada</p>
-            
+            <button onClick={() => navigate('/')} className="px-4 py-2 text-sm font-medium text-white bg-pink-600 border border-transparent rounded-md shadow-sm hover:bg-pink-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-500">Voltar para o Login</button>
         </div>
     )
 }
