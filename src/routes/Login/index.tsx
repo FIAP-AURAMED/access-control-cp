01c8babfad0ba66f986f644803e5ba9674dcc527
IsabelaYamauchi <isa.yamauchi.y@gmail.com>
2025-10-07 21:35:33 -0300

    Validação do Login e conectado com a api

--- src/routes/Login/index.tsx
+++ src/routes/Login/index.tsx
@@ -1,7 +1,35 @@
-
-
+import Form from "../../components/Form";
+import type { TipoUsuario } from "../../types/tipoUsu";
+import { Link, useNavigate } from "react-router-dom";
+const API_URL = import.meta.env.VITE_API_URL_BASE;
+ 
+ 
 export default function Login() {
+ 
+    const navigate = useNavigate();
+ 
+    const onSubmit = async (data: TipoUsuario) => {
+        try {
+            const response = await fetch(`${API_URL}`);
+ 
+            if (!response.ok) {
+                throw new Error('Erro ao buscar usuários');
+            }
+ 
+            
+ 
+            
+        } catch (error) {
+            console.error('Falha no login:', error);
+            alert('Ocorreu um erro ao tentar fazer login.');
+        }
+ 
+ 
+    };
+ 
     return (
-        <div></div>
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+            
+        </div>
     )
 }
