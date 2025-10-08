f4a52b2016043e3aeebd5bb99546db2a64d8e34e
grazialencar <graziellealencar3@gmail.com>
2025-10-07 22:08:52 -0300

    Mensagem de erro do email ajustada

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -37,7 +37,7 @@ export default function Form({ onSubmit, fields, buttonText, legend }: FormProps
                 {fields.includes('email') && (
                     <div>
                         <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email</label>
-                        <input type="email" id="email" placeholder="Digite seu e-mail" className="block w-full text-sm px-4 py-2 mt-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-pink-500 focus:border-pink-500" {...register('email', { required: 'O nome de usuário é obrigatório' })} />
+                        <input type="email" id="email" placeholder="Digite seu e-mail" className="block w-full text-sm px-4 py-2 mt-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-pink-500 focus:border-pink-500" {...register('email', { required: 'O email é obrigatório' })} />
                         {errors.email && <small className="mt-2 text-sm text-red-600">{errors.email.message}</small>}
                     </div>
                 )}
